13d5278ce6ebc4764b8d31ec5f75a73858745d19
clyde0813 <eric3285@naver.com>
2022-09-22 13:15:50 +0900

    db -> sqlite3

--- database_01/settings.py
+++ database_01/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,12 +74,8 @@ WSGI_APPLICATION = 'database_01.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'C##CLYDE0813',
-        'USER': 'C##CLYDE0813',
-        'PASSWORD': 'k9KXxKSJcF87bF5hMq6EBer8G',
-        'HOST': '114.70.93.100',
-        'PORT': '7772',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
